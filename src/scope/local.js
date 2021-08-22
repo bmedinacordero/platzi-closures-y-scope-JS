f0d648ecc5a5a7dbd2f4e477e75dd8d7a4eeee35
Bryan Medina <bmedinacordero2@gmail.com>
2021-08-22 01:25:23 -0400

    function scope

--- src/scope/local.js
+++ src/scope/local.js
@@ -1,8 +1,20 @@
-const helloWorld = () =>{
-    const hello = "Hello World";
-    console.log(hello);
-}
+// const helloWorld = () =>{
+//     const hello = "Hello World";
+//     console.log(hello);
+// }
 
-helloWorld();
+// helloWorld();
 
 console.log(hello);
+
+var scope = "i am global";
+
+const functionScope = () => {
+    var scope = "i am just a local";
+    const func = () =>{
+        return scope
+    }
+console.log(func());
+};
+
+functionScope();
